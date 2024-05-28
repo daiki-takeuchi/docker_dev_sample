73ac0ac307284c81b58e7d3bde0dd98bbaa825e5
daiki.takeuchi <daiki.takeuchi.0327@gmail.com>
2024-05-28 14:33:19 +0900

    first commit

---
+++ src/main/java/com/example/docker_dev_sample/DockerDevSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.docker_dev_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDevSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDevSampleApplication.class, args);
+    }
+
+}
